cabde63efa657680acb37a1ad3dc100fc59e7ad6
Laus Wullum <laus.w@hotmail.com>
2022-06-09 18:30:55 +0200

    Final Working State yielding a latex file with the tableproducing code

--- appliedTest.jl
+++ appliedTest.jl
@@ -14,52 +14,54 @@ include("semiParam3.jl")
 #########################################################################
 # APPLIED STUDY
 #########################################################################
-len(x) = (q = round.(x, digits = 4), len = round(x[2] - x[1], digits = 3))
 # Load the data in
 root = dirname(@__FILE__)
 DF = CSV.read(root * "/exercise3data.csv", DataFrame, header = [:ID, :Y, :R, :X], skipto = 2)
 
-print("next ££££££££££££££££££££ \n")
-# A first naive estimate (A.a)
+
+#########################################################################
+# Produce dataframe
+#########################################################################
+
 fitwithout = glm(@formula(Y ~ R), DF, Binomial(), LogitLink())
-coef(fitwithout)[2]
-confint(fitwithout)[2, :] |> len
+coefGLM = coef(fitwithout)[2]
+confintGLMLower, confintGLMUpper = confint(fitwithout)[2, :]
 
-print("next ££££££££££££££££££££\n")
-# Naive estimate using the naive influence function (don't report)
-βhat, seβhat = naiveEst(DF.R, DF.Y, DF.X)
-quantile.(Normal(βhat,seβhat ), [0.025, 0.975]) |> len
-
-print("next ££££££££££££££££££££ \n")
-# The efficient estimate (A.b)
 βhatEff, seβhatEff = effEst(DF.R, DF.Y, DF.X)
-quantile.(Normal(βhatEff,seβhatEff), [0.025, 0.975]) |> len
+confintHatEffLower, confintHatEffUpper = quantile.(Normal(βhatEff,seβhatEff), [0.025, 0.975])
 
-print("next ££££££££££££££££££££\n")
-# The efficient estimate (A.c)
 βtilde, seβtilde =  polEst(DF.R, DF.Y, DF.X)
-quantile.(Normal(βtilde,seβtilde), [0.025, 0.975]) |> len
+confintPolLower, confintPolUpper =  quantile.(Normal(βtilde,seβtilde), [0.025, 0.975]) 
 
-print("next ££££££££££££££££££££\n")
-# The efficient estimate (A.c)
-βmis, seβmis =  effMisEst(DF.R, DF.Y, DF.X)
-quantile.(Normal(βmis,seβmis), [0.025, 0.975]) |> len
+
+DF = DataFrame(
+    type = ["GLM", "Efficient", "Polynomial"],
+    estimate = [coefGLM, βhatEff, βtilde], 
+    lower = [confintGLMLower, confintHatEffLower,confintPolLower],
+    upper = [confintGLMUpper, confintHatEffUpper,confintPolUpper]
+)
 
 #########################################################################
 # Produce tables to latex using GT in R
 #########################################################################
 
+# Get path of current file
+root = dirname(@__FILE__)
 
+# Put the full dataframe and path to R using RCall.jl
+@rput DF
+@rput root
 
-#xWhereY1 = filter(row -> row.Y == 1, DF)
-#xWhereY0 = filter(row -> row.Y == 0, DF)
-###
-#violin(["0"], xWhereY0.X, label = "no outcome")
-#violin!(["1"],xWhereY1.X, label = "outcome")
-##
-#mean(xWhereY0.R) 
-#mean(xWhereY1.R)
-#
-#histogram(xWhereY1.R, label = "outcome", normalize = true)
-#histogram!(xWhereY0.R, label = "no outcome", normalize = true)
-#
+R"""
+tibble(DF) |>
+    gt() |>
+    fmt_number(
+        columns = 2:4,
+        decimals = 3
+    ) |>
+    tab_spanner(label = "confidence interval", columns = c("lower", "upper")        
+    ) |>
+    as_latex() |>
+    as.character() |>
+    writeLines(con = paste(root, "/latex_table_est.tex", sep = ""))
+"""
